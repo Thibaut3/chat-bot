eb102efd850ddff387bbaa32092fffeb9e689cf1
Thibaut3 <56863170+Thibaut3@users.noreply.github.com>
2025-04-02 15:23:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    print("ICI",os.environ)
     return render_template('index.html')
 
 if __name__ == '__main__':
-    print(os.environ)
+    print("ICI",os.environ)
     app.run(debug=True)
